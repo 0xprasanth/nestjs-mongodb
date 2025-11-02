0896296a46be8d0504d9ad85749ac50b4c022bf2
0xprasanth <raviprasanth45@gmail.com>
2025-11-02 18:17:21 +0530

    feat: add @nestjs/jwt and update app module for authentication integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import * as dotenv from 'dotenv';
 import { UserModule } from './users/users.modules';
 import { PostsModule } from './posts/posts.module';
-dotenv.config({ path: process.cwd() + `/.env.${process.env.NODE_ENV}` });
+import { AuthModule } from './auth/auth.module';
+// import * as dotenv from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+// dotenv.config({ path: process.cwd() + `/.env.${process.env.NODE_ENV}` });
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 👈 makes env available in all modules
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'], // fallback
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI!),
     UserModule,
     PostsModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
